ba3ae7093df43de13521a7b682e0e7852f040af1
vvakame <vvakame+dev@gmail.com>
2014-09-17 01:21:24 +0900

    initial commit

---
+++ lib/fs.ts
@@ -0,0 +1,5 @@
+class FSGit {
+
+}
+
+export = FSGit;
---
+++ test/main_spec.ts
@@ -0,0 +1,10 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+/// <reference path="../typings/assert/assert.d.ts" />
+
+import assert = require("power-assert");
+
+describe("describe", ()=> {
+    it("it", ()=> {
+        assert("" === "");
+    });
+});
