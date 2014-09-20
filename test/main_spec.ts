140297cc210a6355ad70acb87560da47d670ef3e
vvakame <vvakame+dev@gmail.com>
2014-09-20 22:10:05 +0900

    improve test suite

--- test/main_spec.ts
+++ test/main_spec.ts
@@ -7,42 +7,107 @@
 import assert = require("power-assert");
 import fsgit = require("../lib/fs");
 
+import fs = require("fs");
+import child_process = require("child_process");
+
 describe("fs-git", ()=> {
-    it("open master branch", ()=> {
-        return fsgit.open("./test/fixture/bare", "master").then(fs => {
-            var filelist = fs.filelist().then(files=> {
-                assert(files.length !== 0);
-                assert(files.filter(file=> file.path === ".zshrc").length === 1);
+    before(done => {
+        if (fs.existsSync("./test/fixture/vanilla")) {
+            done();
+            return;
+        }
+        child_process.exec("git clone test/fixture/bare.git test/fixture/vanilla", ()=> {
+            child_process.exec("git checkout -b develop origin/develop", {cwd: "test/fixture/vanilla"}, ()=> {
+                done();
             });
-            var readFile = fs.readFile(".zshrc", {encoding: "utf8"}).then(content=> {
-                assert(typeof content === "string");
-            });
-            var exists1 = fs.exists(".zshrc").then(exists=> {
-                assert(exists === true);
-            });
-            var exists2 = fs.exists("notExists").then(exists=> {
-                assert(exists === false);
-            });
-            return Promise.all([filelist, readFile, exists1, exists2]);
         });
     });
 
-    it("open specific ref", ()=> {
-        return fsgit.open("./test/fixture/bare", "ae6a41").then(fs => {
-            var filelist = fs.filelist().then(files=> {
-                assert(files.length !== 0);
-                assert(files.filter(file=> file.path === ".zshrc").length === 1);
+    var matrix:{name: string; gitDir: string; }[] = [
+        {
+            name: "bare repository",
+            gitDir: "./test/fixture/bare.git"
+        },
+        {
+            name: "cloned repository",
+            gitDir: "./test/fixture/vanilla/.git"
+        }
+    ];
+    matrix.forEach(dirInfo => {
+        it("open master branch in " + dirInfo.name, ()=> {
+            return fsgit.open(dirInfo.gitDir, "master").then(fs => {
+                var filelist = fs.filelist().then(files=> {
+                    assert(files.length !== 0);
+                    assert(files.filter(file=> file.path === "master-branch-only.js").length === 1);
+                });
+                var readFile = fs.readFile("subdir2/master-branch-only.js", {encoding: "utf8"}).then(content=> {
+                    assert(typeof content === "string");
+                });
+                var exists1 = fs.exists("subdir2/master-branch-only.js").then(exists=> {
+                    assert(exists === true);
+                });
+                var exists2 = fs.exists("develop-branch-only.js").then(exists=> {
+                    assert(exists === false);
+                });
+                return Promise.all([filelist, readFile, exists1, exists2]);
             });
-            var readFile = fs.readFile(".zshrc", {encoding: "utf8"}).then(content=> {
-                assert(typeof content === "string");
+        });
+
+        it("open develop branch in " + dirInfo.name, ()=> {
+            return fsgit.open(dirInfo.gitDir, "develop").then(fs => {
+                var filelist = fs.filelist().then(files=> {
+                    assert(files.length !== 0);
+                    assert(files.filter(file=> file.path === "develop-branch-only.js").length === 1);
+                });
+                var readFile = fs.readFile("subdir/develop-branch-only.js", {encoding: "utf8"}).then(content=> {
+                    assert(typeof content === "string");
+                });
+                var exists1 = fs.exists("develop-branch-only.js").then(exists=> {
+                    assert(exists === true);
+                });
+                var exists2 = fs.exists("master-branch-only.js").then(exists=> {
+                    assert(exists === false);
+                });
+                return Promise.all([filelist, readFile, exists1, exists2]);
             });
-            var exists1 = fs.exists(".zshrc").then(exists=> {
-                assert(exists === true);
+        });
+
+        it("open test-tag tag in " + dirInfo.name, ()=> {
+            return fsgit.open(dirInfo.gitDir, "test-tag").then(fs => {
+                var filelist = fs.filelist().then(files=> {
+                    assert(files.length !== 0);
+                    assert(files.filter(file=> file.path === "subdir/test.txt").length === 1);
+                });
+                var readFile = fs.readFile("subdir/test.txt", {encoding: "utf8"}).then(content=> {
+                    assert(typeof content === "string");
+                });
+                var exists1 = fs.exists("subdir/test.txt").then(exists=> {
+                    assert(exists === true);
+                });
+                var exists2 = fs.exists("master-branch-only.js").then(exists=> {
+                    assert(exists === false);
+                });
+                return Promise.all([filelist, readFile, exists1, exists2]);
             });
-            var exists2 = fs.exists("notExists").then(exists=> {
-                assert(exists === false);
+        });
+
+        it("open specific ref in " + dirInfo.name, ()=> {
+            return fsgit.open(dirInfo.gitDir, "b41735").then(fs => {
+                var filelist = fs.filelist().then(files=> {
+                    assert(files.length !== 0);
+                    assert(files.filter(file=> file.path === "subdir/test.txt").length === 1);
+                });
+                var readFile = fs.readFile("subdir/test.txt", {encoding: "utf8"}).then(content=> {
+                    assert(typeof content === "string");
+                });
+                var exists1 = fs.exists("subdir/test.txt").then(exists=> {
+                    assert(exists === true);
+                });
+                var exists2 = fs.exists("master-branch-only.js").then(exists=> {
+                    assert(exists === false);
+                });
+                return Promise.all([filelist, readFile, exists1, exists2]);
             });
-            return Promise.all([filelist, readFile, exists1, exists2]);
         });
     });
 });
